d13304ee2981ee4d86fbab511a894980ee2fa903
VISHEPE <emmanuelodh428@gmail.com>
2024-10-22 02:38:36 -0700

    map

---
+++ maps.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Creating a Map with String keys and int values
+  Map<String, int> carOwner = {
+    'Alice': 30,
+    'Bob': 25,
+    'Charlie': 35,
+  };
+  print("Ages of students: $carOwner");
+}
